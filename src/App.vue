1337f3debace882b50e6dd44dd84ccb474020b24
Pattama <par_087v@hotmail.com>
2020-12-03 14:06:50 +0700

    update55

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Kanban/>
+    <Kanban :data="data"/>
   </div>
 </template>
 
@@ -11,6 +11,27 @@ export default {
   name: 'App',
   components: {
     Kanban
+  },
+  data(){
+    return{
+      data:[
+        {
+          name:"todo",
+          color:"#e3ec58",
+          task:[]
+        },
+         {
+          name:"doing",
+          color:"#58ec78",
+          task:[]
+        },
+         {
+          name:"done",
+          color:"#4be7e3",
+          task:[]
+        }
+      ]
+    }
   }
 }
 </script>
--- src/components/kanban.vue
+++ src/components/kanban.vue
@@ -1,12 +1,32 @@
 <template>
 <div class="Kanban">
-      Kanban
+      <div class="Column" :style="{backgroundColor:column.color}" v-for="(column , index) in data" :key="index">
+          <div class="Column-header">
+              {{column.name}}
+          </div>
+          <div class="Column-body">
+              <div class="Create-task" @click="create_task(index)">
+              Create Task
+              </div> 
+          </div>
+      </div>
+       <b-modal ref="create-task-modal" title="Create Task">
+          <p class="my-4">Hello from modal!</p>
+          </b-modal>
 </div>
 </template>
 
 <script>
 export default {
-
+   props:{
+       data:Array
+   },
+   methods:{
+       create_task(index_column){
+           console.log(index_column);
+         this.$refs["create-task-modal"].show()
+       },
+   },
 }
 </script>
 
@@ -15,5 +35,41 @@ export default {
     width: 100%;
     height: 100%;
     background-color: aquamarine;
+}
+.Column{
+    height: 600px;
+    width: 300px;
+    border-radius: 10px;
+    display: inline-block;
+    margin: 25px;
+    -webkit-box-shadow: 9px 10px 6px -2px rgba(0,0,0,0.42);
+    -moz-box-shadow: 9px 10px 6px -2px rgba(0,0,0,0.42);
+    box-shadow: 9px 10px 6px -2px rgba(0,0,0,0.42);
+    padding: 15px;
+}
+.Column-header{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+    font-size: 32px;
+    font-weight: bold;
+}
+.Column-body{
+    height: calc(100% - 50px);
+    border-radius: 10px;
+    padding: 5px;
+    background-color: rgba(255, 255, 255, 0.678);
+    
+}
+.Create-task{
+    width: 100%;
+    height: auto;
+    padding: 10px;
+    border-radius: 5px;
+    cursor: pointer;
+}
+.Create-task:hover{
+    background-color: rgba(53, 35, 44, 0.411);
 }
 </style>
